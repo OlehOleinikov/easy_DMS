a6c1243b1b04944c91e9661ba946216f2a70eaf5
oleh_oleinikov <oleh.oleynikov@gmail.com>
2023-03-03 20:39:42 +0200

    exception add

--- main.py
+++ main.py
@@ -68,12 +68,12 @@ for file_index in tqdm(range(len(files_in_dir)), bar_format='{l_bar}{bar:45}{r_b
         person_photo = None
 
     # Convert data to instance
-    #try:
-    cur_person = PersonProfile(data_ukr, data_eng, person_photo, cur_file)
-    person_cards.append(cur_person)
-    status = True
-    #except Exception as err:
-        #errors_dict.update({cur_file: f'Помилка збору відомостей: {str(err)}'})
+    try:
+        cur_person = PersonProfile(data_ukr, data_eng, person_photo, cur_file)
+        person_cards.append(cur_person)
+        status = True
+    except Exception as err:
+        errors_dict.update({cur_file: f'Помилка збору відомостей: {str(err)}'})
 
     if status:
         string_report += f"[#00FF00]   OK   [#FF00FF]file: {cur_file}\n"
